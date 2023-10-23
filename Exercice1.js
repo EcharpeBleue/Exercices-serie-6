488054827927ca8d9710474f0b5f320e6b884dff
EcharpeBleue <zawiczrczarny@gmail.com>
2023-10-23 13:20:33 +0200

    Initial commit

---
+++ Exercice1.js
@@ -0,0 +1,9 @@
+let tableauEntiers = new Array
+nbreVoulus = prompt(`Veuillez entrer le nombre de chiffres que vous voulez`)
+for (i=0;i<nbreVoulus;i++)
+{
+    tableauEntiers.push = parseInt(prompt(`Veuillez entrer les valeurs souhaités`))
+}
+tableauEntiers.forEach(employe => {
+    console.log(employe)
+});
---
+++ README.md
@@ -0,0 +1,2 @@
+# Exercices-serie-6
+ 
